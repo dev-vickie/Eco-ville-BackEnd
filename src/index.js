579b324b9e7b4582f57edaa3bf3ab3907941d5e3
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-12-06 15:54:40 +0300

    added the trust proxy

--- src/index.js
+++ src/index.js
@@ -33,6 +33,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('trust proxy', 1)
 app.use(
   session({
     secret: "keyboard cat",
