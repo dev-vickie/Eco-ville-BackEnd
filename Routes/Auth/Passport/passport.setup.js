88437a1be334dd8b2f27c3c76bfc716381c83f13
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-12-07 00:30:09 +0300

    added the authentication passport for google and twitter

--- Routes/Auth/Passport/passport.setup.js
+++ Routes/Auth/Passport/passport.setup.js
@@ -1,6 +1,50 @@
 import passport from "passport";
 import GoogleStrategy from "passport-google-oauth2";
+import TwitterStrategy from "passport-twitter";
+
 import dotenv from "dotenv";
 dotenv.config();
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function (user, done) {
+    done(null, user);
+  });
+  
+
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL,
+        passReqToCallback: true,
+        proxy:false
+      },
+      function (request, accessToken, refreshToken, profile, done) {
+        console.log(profile);
+        return done(null, profile);
+      }
+    )
+  );
+  
+  passport.use(
+    new TwitterStrategy(
+      {
+        consumerKey: process.env.TWITTER_CONSUMER_KEY,
+        consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+        callbackURL: process.env.TWITTER_CALLBACK_URL,
+        passReqToCallback: true,
+        proxy: false,
+      },
+      function (request, accessToken, refreshToken, profile, done) {
+        console.log(profile);
+        return done(null, profile);
+      }
+    )
+  );
+  
 
 export default passport;
