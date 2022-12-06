9cf3a7435bead456ffc9152a70ad7aab3ec22c6a
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-12-06 14:13:54 +0300

    added the setup settings for passport

---
+++ Routes/Auth/Passport/passport.setup.js
@@ -0,0 +1,27 @@
+import passport from "passport";
+import GoogleStrategy from "passport-google-oauth2";
+import dotenv from "dotenv";
+dotenv.config();
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+    });
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+    });
+
+passport.use(
+  new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    passReqToCallback: true,
+  },function(request, accessToken, refreshToken, profile, done) {
+    console.log(profile);
+    return done(null, profile);
+  }
+  
+  )
+);
+
