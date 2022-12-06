c615203f12451a4e42b84f7ef2ad5ff566e814cc
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-12-06 14:13:19 +0300

    added the configurations for passport and express session

--- src/index.js
+++ src/index.js
@@ -11,7 +11,12 @@ import payRouter from "../Routes/Mpesa/mpesa.js";
 import profileRouter from "../Routes/profile/profile.js";
 import verifyRouter from "../Routes/User/verify.js";
 import feedbackRouter from "../Routes/Feedback/feedback.js";
+import passport from "passport";
+import '../Routes/Auth/passport/passport.setup.js';
+import { profile } from "console";
+import session from "express-session";
 export const app = express();
+
 
 app.set("view engine", "ejs");
 
@@ -19,14 +24,19 @@ app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/auth", (req, res) => {
-  res.render("pages/auth");
-});
 
 app.all("/", (req, res) => {
   res.send("This is the home page");
@@ -36,6 +46,26 @@ app.post("/register", createUser);
 app.post("/login", signInUser);
 app.post("/remove", protect, removeAccount);
 app.put("/changePassword", protect, changePassword);
+app.get('/auth', passport.authenticate('google', { scope: ['profile', 'email'] }));
+app.get('/auth/google', passport.authenticate('google', {failureRedirect: "/error"}), async(req, res)=>{
+  const firstName = req.user.name.givenName;
+  const lastName = req.user.name.familyName;
+  const email = req.user.emails[0].value;
+  await prisma.user.create({
+    data: {
+      firstName: firstName,
+      lastName: lastName,
+      contactEmail: email,
+      password: "pass123",
+    }});
+  res.json({firstName, lastName, email, password});
+});
+
+
+app.get('/success', (req,res)=>{
+  res.json({firstName, lastName, email, password});
+})
+
 
 app.use("/post", protect, postRouter);
 app.use("/order", protect, orderRouter);
